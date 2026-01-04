ec9c85398cd70952a0771fc17124a111f6b51729
ronAa21 <aaronpagente19@gmail.com>
2026-01-04 11:26:01 +0800

    Another push for the routes

--- Cash_Flow/logging_page/credentials-retriever.js
+++ Cash_Flow/logging_page/credentials-retriever.js
@@ -47,7 +47,7 @@ document.querySelector(".submit").addEventListener("click", function(e) {
     return;
   }
 
-  fetch("http://localhost:4001/project/signup", {
+  fetch("https://project-s-nhsi.onrender.com/project/signup", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
